79472be72b1eaad91514f7efd68f7a091dec8e6d
detarso7 <de_tarso7@hotmail.com>
2023-02-08 15:49:11 -0300

    Correção

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -4,26 +4,22 @@ import{
     DiJsBadge,
     DiNodejsSmall,
     DiMysql,
-    DiReact,
-    DiPhotoshop, DiSass
+    DiReact
 } from 'react-icons/di'
-import {SiTypescript,} from 'react-icons/si'
-import FiFigma from 'react-icons/fi'
 
 import '../styles/components/technologiescontainer.sass'
 
 const technologies = [
-    {id: "figma", nome:"Figma", texto: "Proficiência em edição de imagens e criação gráfica.", icon: <FiFigma/>},
-    {id: "photo", nome:"Photoshop", texto: "Experiência em design e colaboração de interface de usuário.", icon: <DiPhotoshop/>},
+    {id: "figma", nome:"Figma", texto: "Proficiência em edição de imagens e criação gráfica."},
+    {id: "photo", nome:"Photoshop", texto: "Experiência em design e colaboração de interface de usuário."},
     {id: "html", nome:"HTML5", texto: "Experiência em codificação web estrutural e semanticamente correta.", icon: <DiHtml5/>},
     {id: "css", nome:"CSS3", texto: "Habilidade em estilo e formatação de páginas web.", icon: <DiCss3/>},
-    {id: "sass", nome:"SASS", texto: "Conhecimento em pré-processador CSS avançado..", icon: <DiSass/>},
+    {id: "sass", nome:"SASS", texto: "Conhecimento em pré-processador CSS avançado.."},
     {id: "js", nome:"JavaScript", texto: "Proficiência em linguagem de programação web interativa.", icon: <DiJsBadge/>},
-    {id: "type", nome:"Typescript", texto: "Habilidade em tipagem estática para JavaScript.", icon: <SiTypescript/>},
+    {id: "type", nome:"Typescript", texto: "Habilidade em tipagem estática para JavaScript."},
     {id: "react", nome:"React", texto: "Proficiência em biblioteca JavaScript para aplicativos da web.", icon: <DiReact/>},
     {id: "node", nome:"Node.JS", texto: "Experiência em plataforma JavaScript para aplicações de servidor.", icon: <DiNodejsSmall/>},
     {id: "mysql", nome:"MySql", texto: "Habilidade em gerenciamento de banco de dados relacional.", icon: <DiMysql/>}
-    
 ]
 
 const TechnologiesContainer = () => {
@@ -40,7 +36,8 @@ const TechnologiesContainer = () => {
                     {tech.icon}
                     <div className="technologies-info">
                         <h3>{tech.nome}</h3>
-                        <p>{tech.texto}</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </p>
                     </div>
                 </div>
 
