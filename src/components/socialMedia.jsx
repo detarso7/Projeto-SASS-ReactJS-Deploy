8064c946d6d57a47492c744730cc222c39c00573
Saulo de Tarso <38414640+detarso7@users.noreply.github.com>
2023-02-14 21:15:32 -0300

    ..

--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -1,11 +1,11 @@
-import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa"
+import { FaLinkedinIn, FaGithub, FaBehance } from "react-icons/fa"
 import '../styles/components/socialmedia.sass'
 
 const socialMedia = [
 
     {name: "Linkedin", icon: <FaLinkedinIn/>, link: "https://www.linkedin.com/in/saulo-de-tarso/"},
     {name: "Github", icon: <FaGithub/>, link: "https://github.com/detarso7"},
-    {name: "Instagram", icon: <FaInstagram/>, link: "https://www.instagram.com/saulo_d_tarso/"}
+    {name: "Behance", icon: <FaBehance/>, link: "https://www.behance.net/de_tarso7e982"}
 
 ];
 
